62399580c138cc3ec44032032e9bbb46474d023b
Marta <Marta@Marta.P-661HNU-F1>
2016-09-27 23:20:17 +0300

    Exercise Finished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,39 @@
+import static java.lang.Integer.parseInt;
 
 public class SudokuVerifier {
-	
-	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
-		
-		//check
-	}
+
+    public int verify(String candidateSolution) {
+        boolean b = true;
+        int valor = 0;
+        for (int i = 0; i < candidateSolution.length(); i++) {
+            if (!(Integer.parseInt(candidateSolution.substring(i, i + 1)) > 0 && Integer.parseInt(candidateSolution.substring(i, i + 1)) <= 9)) {
+                b = false;
+            }
+        }
+        if (!b) {
+            valor = -1;
+        }
+
+        /* for (int i = 0; i < candidateSolution.length(); i++) {
+            if (!(Integer.parseInt(candidateSolution.substring(i, i + 1)) > 0 && Integer.parseInt(candidateSolution.substring(i, i + 1)) <= 9)) {
+                b = false;
+            }
+        }
+        if (!b) {
+            valor = -2;
+        } */
+
+        if (b) {
+            for (int i = 0; i <= 72; i = i + 9) {
+                int j = i + 9;
+                if ()  {
+                    b = false;
+                }
+            }
+            if (!b) {
+                valor = -3;
+            }
+            return valor;
+        }
+    }
 }
